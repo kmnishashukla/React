a7a8d82662c46e3eae5120d261b82e0b2a15e36e
kmnishashukla <66797037+kmnishashukla@users.noreply.github.com>
2025-12-03 07:13:35 +0530

    Redux setup

---
+++ Practicals/WScube/createModule/calculator.js
@@ -0,0 +1,10 @@
+export let sumData = (a,b) => {
+    return a + b;
+}
+
+export let minData = (a,b) => {
+    return a -b;
+}
+
+export let cName = "nisha";
+// export  {sumData,minData};
---
+++ Practicals/WScube/createModule/index.js
@@ -0,0 +1,6 @@
+import {sumData as sum, minData, cName} from "./calculator.js";
+console.log(sum(1,30));
+
+console.log(minData(30,1))
+
+console.log(cName);
